9ab0b6d788bde84cbb51f0cafea2d5c58bb842d9
Burak Küçükcengiz <burakkucukcengiz@Burak-MacBook-Pro.local>
2025-12-24 17:58:52 +0300

    Inventory business logic and core structure implemented

---
+++ JavaProjec/deneme.java
@@ -0,0 +1,7 @@
+package JavaProjec;
+
+public class deneme {
+    public static void main(String[] args) { 
+        System.out.println("Java project"); 
+    }
+}
---
+++ JavaProjec/deneme1.java
@@ -0,0 +1,11 @@
+package JavaProjec;
+
+public class deneme1 {
+    public static void main(String[] args) { 
+
+    int a=5 ;
+    int b=16 ;
+   int  z = a + b;
+    System.out.println("Toplam" + z);
+}
+}
